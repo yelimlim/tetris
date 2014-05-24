2d68a1b92608a7cf86688fb21e0ffa93414694c5
mong <yelimlim@naver.com>
2014-05-24 17:33:32 +0900

    project

---
+++ week11/week11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(new FileStream("C:/Users/mong/Desktop/pp.txt", FileMode.Create));
+            sw.WriteLine("프로그래밍 연습");
+            sw.WriteLine("C# 오전 분반");
+            sw.WriteLine("--... 피곤 하다");
+            sw.Close();
+
+            StreamReader sr = new StreamReader(new FileStream("pp.txt", FileMode.Open));
+
+            while (sr.EndOfStream == false)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+        }
+    }
+}
